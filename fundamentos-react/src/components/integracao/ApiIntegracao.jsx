15b275caf9ddff55c8b9958d7108a622fd662f19
Glauber M. da Silva <gmsoft.informatica@gmail.com>
2020-05-31 08:29:48 -0300

    Adição de CSS na api de integração

--- fundamentos-react/src/components/integracao/ApiIntegracao.jsx
+++ fundamentos-react/src/components/integracao/ApiIntegracao.jsx
@@ -1,24 +1,36 @@
-import './ApiIntegracao.css'
+import "./ApiIntegracao.css";
 import React, { useState, useEffect } from "react";
 
 export default (props) => {
   const [repositorios, setRepositorios] = useState([]);
 
-  useEffect(async () => {
-    const response = await fetch(
-      "https://api.github.com/users/glaubermarcelino/repos"
-    );
-    const data = await response.json();
-
-    setRepositorios(data);
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        "https://api.github.com/users/glaubermarcelino/repos"
+      );
+      const data = await response.json();
+      setRepositorios(data);
+    }
+    fetchData();
   }, []);
 
+  function handleFavorite(id) {
+    const newRepositories = repositorios.map((repo) => {
+      return repo.id === id ? { ...repo, favorite: !repo.favorite } : repo;
+    });
+    setRepositorios(newRepositories);
+  }
   return (
     <>
       <h3>GITHUB Repositories</h3>
       <ul className="repository">
         {repositorios.map((repo) => (
-          <li key={repo.id}>{repo.name}</li>
+          <li key={repo.id}>
+            {repo.name}
+            {repo.favorite && <span>(Favorito)</span>}
+            <button onClick={() => handleFavorite(repo.id)}>Favoritar</button>
+          </li>
         ))}
       </ul>
     </>
